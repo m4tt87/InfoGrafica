06c38cfd3900ed317ab2a80458989b57b0caa11d
Mattia Ciriolo <mattiaciriolo@gmail.com>
2013-02-14 20:07:48 +0100

    It isn't a library project

--- RecomSysAppClient/gen/com/recsysclient/R.java
+++ RecomSysAppClient/gen/com/recsysclient/R.java
@@ -11,90 +11,90 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static int applogo2=0x7f020000;
-        public static int ic_launcher=0x7f020001;
+        public static final int applogo2=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static int button_lancia_servizio=0x7f070012;
-        public static int button_vota_evento=0x7f07000d;
-        public static int descrizione_evento_tv=0x7f07000a;
-        public static int descrizione_servizio_tv=0x7f070011;
-        public static int eventList=0x7f070002;
-        public static int event_icon=0x7f070006;
-        public static int event_name=0x7f070007;
-        public static int label_titolo_lista_eventi=0x7f070000;
-        public static int label_titolo_lista_servizi=0x7f070003;
-        public static int linearLayout1=0x7f070013;
-        public static int map=0x7f070026;
-        public static int nessun_evento_tv=0x7f070001;
-        public static int nessun_servizio_tv=0x7f070004;
-        public static int nome_evento_tv=0x7f070009;
-        public static int nome_servizio_tv=0x7f070010;
-        public static int ratingBar1=0x7f07000c;
-        public static int serviceList=0x7f070005;
-        public static int tableRow1=0x7f070024;
-        public static int tableRow2=0x7f070025;
-        public static int tableRow3=0x7f070027;
-        public static int textView1=0x7f070008;
-        public static int toggleButton=0x7f070014;
-        public static int toggleButton2=0x7f070022;
-        public static int toggleButton3=0x7f070023;
-        public static int tv_lista_servizi_evento_label=0x7f07000e;
-        public static int tv_rating_label=0x7f07000b;
-        public static int tv_servizio_label=0x7f07000f;
-        public static int tw_GPS_status=0x7f070016;
-        public static int tw_LocationProvider=0x7f070018;
-        public static int tw_accuratezza=0x7f07001f;
-        public static int tw_label_GPS_status=0x7f070015;
-        public static int tw_label_accuratezza=0x7f07001e;
-        public static int tw_label_location_provider=0x7f070017;
-        public static int tw_label_posizione=0x7f070019;
-        public static int tw_label_posizione_lat=0x7f07001a;
-        public static int tw_label_posizione_long=0x7f07001c;
-        public static int tw_label_velocita=0x7f070020;
-        public static int tw_posizione_lat=0x7f07001b;
-        public static int tw_posizione_long=0x7f07001d;
-        public static int tw_velocita=0x7f070021;
+        public static final int button_lancia_servizio=0x7f070012;
+        public static final int button_vota_evento=0x7f07000d;
+        public static final int descrizione_evento_tv=0x7f07000a;
+        public static final int descrizione_servizio_tv=0x7f070011;
+        public static final int eventList=0x7f070002;
+        public static final int event_icon=0x7f070006;
+        public static final int event_name=0x7f070007;
+        public static final int label_titolo_lista_eventi=0x7f070000;
+        public static final int label_titolo_lista_servizi=0x7f070003;
+        public static final int linearLayout1=0x7f070013;
+        public static final int map=0x7f070026;
+        public static final int nessun_evento_tv=0x7f070001;
+        public static final int nessun_servizio_tv=0x7f070004;
+        public static final int nome_evento_tv=0x7f070009;
+        public static final int nome_servizio_tv=0x7f070010;
+        public static final int ratingBar1=0x7f07000c;
+        public static final int serviceList=0x7f070005;
+        public static final int tableRow1=0x7f070024;
+        public static final int tableRow2=0x7f070025;
+        public static final int tableRow3=0x7f070027;
+        public static final int textView1=0x7f070008;
+        public static final int toggleButton=0x7f070014;
+        public static final int toggleButton2=0x7f070022;
+        public static final int toggleButton3=0x7f070023;
+        public static final int tv_lista_servizi_evento_label=0x7f07000e;
+        public static final int tv_rating_label=0x7f07000b;
+        public static final int tv_servizio_label=0x7f07000f;
+        public static final int tw_GPS_status=0x7f070016;
+        public static final int tw_LocationProvider=0x7f070018;
+        public static final int tw_accuratezza=0x7f07001f;
+        public static final int tw_label_GPS_status=0x7f070015;
+        public static final int tw_label_accuratezza=0x7f07001e;
+        public static final int tw_label_location_provider=0x7f070017;
+        public static final int tw_label_posizione=0x7f070019;
+        public static final int tw_label_posizione_lat=0x7f07001a;
+        public static final int tw_label_posizione_long=0x7f07001c;
+        public static final int tw_label_velocita=0x7f070020;
+        public static final int tw_posizione_lat=0x7f07001b;
+        public static final int tw_posizione_long=0x7f07001d;
+        public static final int tw_velocita=0x7f070021;
     }
     public static final class layout {
-        public static int eventi_list=0x7f030000;
-        public static int evento_item=0x7f030001;
-        public static int info_event_layout=0x7f030002;
-        public static int info_service_layout=0x7f030003;
-        public static int main=0x7f030004;
-        public static int maps=0x7f030005;
+        public static final int eventi_list=0x7f030000;
+        public static final int evento_item=0x7f030001;
+        public static final int info_event_layout=0x7f030002;
+        public static final int info_service_layout=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int maps=0x7f030005;
     }
     public static final class raw {
-        public static int end_rec=0x7f050000;
-        public static int start_rec=0x7f050001;
+        public static final int end_rec=0x7f050000;
+        public static final int start_rec=0x7f050001;
     }
     public static final class string {
-        public static int app_name=0x7f060000;
-        public static int hello_world=0x7f060010;
-        public static int label_GPS_status=0x7f060002;
-        public static int label_accuratezza=0x7f060006;
-        public static int label_altitudine=0x7f060007;
-        public static int label_avg_acc=0x7f06000a;
-        public static int label_avg_gyr=0x7f06000b;
-        public static int label_direzione_moto=0x7f06000c;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060010;
+        public static final int label_GPS_status=0x7f060002;
+        public static final int label_accuratezza=0x7f060006;
+        public static final int label_altitudine=0x7f060007;
+        public static final int label_avg_acc=0x7f06000a;
+        public static final int label_avg_gyr=0x7f06000b;
+        public static final int label_direzione_moto=0x7f06000c;
         /**  Layout Main 
          */
-        public static int label_location_provider=0x7f060001;
-        public static int label_posizione=0x7f060003;
-        public static int label_posizione_lat=0x7f060004;
-        public static int label_posizione_long=0x7f060005;
-        public static int label_stato_moto=0x7f060009;
-        public static int label_stato_uso=0x7f06000d;
-        public static int label_velocita=0x7f060008;
-        public static int map_not_ready=0x7f060013;
-        public static int menu_settings=0x7f060011;
-        public static int title_activity_maps=0x7f060012;
+        public static final int label_location_provider=0x7f060001;
+        public static final int label_posizione=0x7f060003;
+        public static final int label_posizione_lat=0x7f060004;
+        public static final int label_posizione_long=0x7f060005;
+        public static final int label_stato_moto=0x7f060009;
+        public static final int label_stato_uso=0x7f06000d;
+        public static final int label_velocita=0x7f060008;
+        public static final int map_not_ready=0x7f060013;
+        public static final int menu_settings=0x7f060011;
+        public static final int title_activity_maps=0x7f060012;
         /**  Layout eventi_list 
          */
-        public static int titolo_lista_eventi=0x7f06000e;
-        public static int titolo_lista_servizi=0x7f06000f;
+        public static final int titolo_lista_eventi=0x7f06000e;
+        public static final int titolo_lista_servizi=0x7f06000f;
     }
     public static final class xml {
-        public static int filter_nfc=0x7f040000;
+        public static final int filter_nfc=0x7f040000;
     }
 }
